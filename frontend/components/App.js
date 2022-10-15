f570f91506d497a3edb37a4829c3e895033ef7e8
BrentGreer404 <yem1546@gmail.com>
2022-10-15 18:43:23 -0400

    added list and todo components

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import Form from "./Form"
+import TodoList from "./TodoList"
+
+const todos = [
+  {
+    name: "Bark the dogs",
+    id: 123,
+    done: false
+  },
+  {
+    name: "Oink the pigs",
+    id: 234,
+    done: false
+  },{
+    name: "Moo the cows",
+    id: 345,
+    done: false
+  },
+]
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todos
+    }
+  }
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos}></TodoList>
+        <Form></Form>
+        <button>Hide Completed</button>
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
     return (
-      <div>
-        TodoList
+      <div>Todos
+        {console.log(this.props.todos)}
+        {this.props.todos.map(item => (
+          <Todo item={item} key={item.id}></Todo>
+        ))}
       </div>
     )
   }
